b878dff945266e923a09fc835088c65d6aadc266
hecthor0 <hecthorgarcia0@gmail.com>
2018-05-06 22:36:21 -0500

    Validación de e-mail final

--- app/src/main/java/com/example/hector/fixnote/MainActivity.java
+++ app/src/main/java/com/example/hector/fixnote/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.Toast;
 
 
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.mail.Authenticator;
 import javax.mail.Message;
@@ -106,8 +108,8 @@ public class MainActivity extends AppCompatActivity {
 
                     String mail = email.getText().toString();
 
-                    if(mail.isEmpty()){
-                        email.setError("E-mail vacío");
+                    if(!validateEmail(email.getText().toString())) {
+                        email.setError("Invalid Email");
                         email.requestFocus();
 
                     }else{
@@ -150,6 +152,18 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    //Return true if email is valid and false if email is invalid
+    protected boolean validateEmail(String email) {
+        String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(emailPattern);
+        Matcher matcher = pattern.matcher(email);
+
+
+        return matcher.matches();
     }
 
 }
