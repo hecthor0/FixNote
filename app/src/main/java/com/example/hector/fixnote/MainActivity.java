2d2549d062b4f62da649b14efe3cd51f947d9280
hecthor0 <hecthorgarcia0@gmail.com>
2018-05-10 00:34:06 -0500

    Overflow menu and colors

--- app/src/main/java/com/example/hector/fixnote/MainActivity.java
+++ app/src/main/java/com/example/hector/fixnote/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hector.fixnote;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -152,6 +153,11 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     //Return true if email is valid and false if email is invalid
